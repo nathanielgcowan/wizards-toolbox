af7154a32d14407724417a569c798edee0a163d1
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2026-01-30 17:09:07 -0600

    first commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-3xl font-bold text-black dark:text-zinc-50">
+        Contact Us - This is the contact page.
+      </h1>
+    </div>
+  );
+}
