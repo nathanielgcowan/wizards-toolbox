23bddc912c9f6191448beebe5f5aac5f84ac7b3c
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2026-02-08 20:17:35 -0600

    added in my ruby list

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import { v4 as uuidv4 } from "uuid";
 
 export default function Home() {
   const [data, setData] = React.useState(null);
+  const [rubyList, setRubyList] = React.useState([]);
   const [isLoading, setLoading] = React.useState(true);
-  const [state, useState] = React.useState(() => {
+  const [state, setState] = React.useState(() => {
     const saved = window.localStorage.getItem("myAppData");
     if (saved) {
       return JSON.parse(saved);
@@ -13,7 +14,7 @@ export default function Home() {
       return [];
     }
   });
-  const [newItem, useNewItem] = React.useState({
+  const [newItem, setNewItem] = React.useState({
     id: "",
     checked: false,
     value: 0,
@@ -39,27 +40,46 @@ export default function Home() {
   }, [state]);
 
   function addItem() {
-    useState([...state, { ...newItem, id: uuidv4() }]);
+    setState([...state, { ...newItem, id: uuidv4() }]);
     console.log("Item added:");
   }
   function editItem(e) {
     const newState = state.map((item) =>
       item.id === e.id ? { ...item, checked: !item.checked } : item,
     );
-    useState(newState);
+    setState(newState);
   }
   function deleteItem(id) {
     const newState = state.filter((e) => e.id !== id);
-    useState(newState);
+    setState(newState);
   }
-  // console.log(state);
+  function getPokemon() {
+    console.log("Pokemon saved!");
+    fetch("http://localhost:3001/pokemons")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Saved Pokemon:", data);
+        setRubyList(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching saved Pokemon:", error);
+      });
+  }
+  // console.log(data);
   return (
     <>
-      {isLoading ? <p>Loading...</p> : <p>Data loaded.</p>}
-      {data.map((e) => (
-        <p key={e.name}>{e.name}</p>
-      ))}
-      <h1>Welcome to a Next Js app.</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        data.map((e) => (
+          <p key={e.name}>
+            {e.name}
+            {""}
+            <button onClick={getPokemon}>Save</button>
+          </p>
+        ))
+      )}
+
       <button onClick={addItem}>Add Item</button>
       {state.map((e, i) => {
         // a unique key is required here
@@ -75,6 +95,15 @@ export default function Home() {
           </p>
         );
       })}
+
+      {rubyList.length > 0 && (
+        <div>
+          <h2>Saved Pokemon:</h2>
+          {rubyList.map((pokemon) => (
+            <p key={pokemon.id}>{pokemon.name}</p>
+          ))}
+        </div>
+      )}
     </>
   );
 }
